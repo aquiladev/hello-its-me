5cdc7d59bc74c350bb7bf1ce06f102437fe32e48
Sergii Bomko <1164492+aquiladev@users.noreply.github.com>
2023-11-04 15:54:59 +0000

    add network config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,24 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+const GWEI = 1000 * 1000 * 1000;
+
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
+  solidity: "0.7.6",
+  networks: {
+    zkEvmTestnet: {
+      url: "https://rpc.public.zkevm-test.net",
+      hardfork: "istanbul",
+      blockGasLimit: 8000000,
+      gasMultiplier: 5,
+      gasPrice: 1 * GWEI,
+      chainId: 1442,
+      accounts: process.env.TT_TESTNET_PRIVATE_KEY
+        ? [process.env.TT_TESTNET_PRIVATE_KEY]
+        : undefined,
+      timeout: 120000,
+    },
+  },
 };
 
 export default config;
